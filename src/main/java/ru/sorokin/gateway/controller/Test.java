9dbd44eb87a419371fb6c0bd0c6c535a79555ec1
Valerii <sorvall@mail.ru>
2025-04-14 15:42:10 +0300

    add Build and Deploy Gateway

--- src/main/java/ru/sorokin/gateway/controller/Test.java
+++ src/main/java/ru/sorokin/gateway/controller/Test.java
@@ -1,6 +1,7 @@
 package ru.sorokin.gateway.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class Test {
 
-    ResponseEntity<String> test(@PathVariable("text") String text) {
-        return ResponseEntity.ok(text);
+    @GetMapping
+    ResponseEntity<String> test() {
+        return ResponseEntity.ok("text");
     }
 
 }
