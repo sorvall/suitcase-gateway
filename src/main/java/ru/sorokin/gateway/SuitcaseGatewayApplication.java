3be292cdbf305c87deeb6893a46b449743bfcc70
Valerii <sorvall@mail.ru>
2025-04-14 14:52:51 +0300

    init

---
+++ src/main/java/ru/sorokin/gateway/SuitcaseGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.sorokin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuitcaseGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuitcaseGatewayApplication.class, args);
+        System.out.println("UP!!!");
+    }
+
+}
