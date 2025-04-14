0d6f336c59ac52c70df62bce9379d734f860a720
Valerii <sorvall@mail.ru>
2025-04-14 15:44:25 +0300

    add Build and Deploy Gateway

---
+++ src/test/java/ru/sorokin/gateway/SuitcaseGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sorokin.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuitcaseGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
