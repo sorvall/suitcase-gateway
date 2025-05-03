433d6e519a7f35641fdce0b1e505a27a15299e11
Valerii <sorvall@mail.ru>
2025-05-03 16:44:41 +0300

    boot admin

--- src/main/java/ru/sorokin/gateway/SuitcaseGatewayApplication.java
+++ src/main/java/ru/sorokin/gateway/SuitcaseGatewayApplication.java
@@ -1,9 +1,11 @@
 package ru.sorokin.gateway;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SuitcaseGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuitcaseGatewayApplication.class, args);
