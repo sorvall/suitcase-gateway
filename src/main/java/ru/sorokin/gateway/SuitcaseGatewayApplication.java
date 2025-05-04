8fa6fd4c6bdf1cff95aca268213198c6b6753d19
Valerii <sorvall@mail.ru>
2025-05-04 17:42:04 +0300

    update front

--- src/main/java/ru/sorokin/gateway/SuitcaseGatewayApplication.java
+++ src/main/java/ru/sorokin/gateway/SuitcaseGatewayApplication.java
@@ -1,11 +1,9 @@
 package ru.sorokin.gateway;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 public class SuitcaseGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SuitcaseGatewayApplication.class, args);
