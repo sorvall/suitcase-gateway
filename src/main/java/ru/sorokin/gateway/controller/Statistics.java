466e799f858d0748e958aaba6f1e3af501e84c15
Valerii <sorvall@mail.ru>
2025-05-07 11:19:00 +0300

    add UniqueUserMetrics

--- src/main/java/ru/sorokin/gateway/controller/Statistics.java
+++ src/main/java/ru/sorokin/gateway/controller/Statistics.java
@@ -1,16 +1,15 @@
 package ru.sorokin.gateway.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/mary")
-public class Mery {
+@RequestMapping("/statistics")
+public class Statistics {
     @GetMapping
-    ResponseEntity<String> test() {
-        return ResponseEntity.ok("Я тут");
+    void test() {
+        System.out.println("statistics супер гуд");
     }
 
 }
