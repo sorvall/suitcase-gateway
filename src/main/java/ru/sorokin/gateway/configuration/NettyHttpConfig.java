8105ce3c6a3f38a8075222486b748303ff2ebfd2
Valerii <sorvall@mail.ru>
2025-05-13 16:24:27 +0300

    http->https redirect

--- src/main/java/ru/sorokin/gateway/configuration/NettyHttpConfig.java
+++ src/main/java/ru/sorokin/gateway/configuration/NettyHttpConfig.java
@@ -1,20 +1,21 @@
 package ru.sorokin.gateway.configuration;
 
-import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class NettyHttpConfig {
     @Bean
-    public NettyServerCustomizer nettyServerCustomizer() {
-        return httpServer -> httpServer
+    public WebServerFactoryCustomizer<NettyReactiveWebServerFactory> nettyCustomizer() {
+        return factory -> factory.addServerCustomizers(httpServer -> httpServer
                 .port(80)
-                .handle((req, res) -> {
-                    String host = req.requestHeaders().get("Host");
-                    return res.status(301)
-                            .header("Location", "https://" + host + req.uri())
+                .handle((request, response) -> {
+                    String host = request.requestHeaders().get("Host");
+                    return response.status(301)
+                            .header("Location", "https://" + host + request.uri())
                             .send();
-                });
+                }));
     }
 }
--- src/main/java/ru/sorokin/gateway/configuration/SecurityConfig.java
+++ src/main/java/ru/sorokin/gateway/configuration/SecurityConfig.java
@@ -9,13 +9,12 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchanges -> exchanges.anyExchange().permitAll())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .build(); // редирект делает NettyHttpConfig, не здесь
+                .build();
     }
 }
 
