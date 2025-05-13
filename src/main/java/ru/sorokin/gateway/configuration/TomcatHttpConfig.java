44b637290122b1de026de57f1f8010b6b30b8fc9
Valerii <sorvall@mail.ru>
2025-05-13 13:30:10 +0300

    http->https redirect

---
+++ src/main/java/ru/sorokin/gateway/configuration/TomcatHttpConfig.java
@@ -0,0 +1,17 @@
+package ru.sorokin.gateway.configuration;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatHttpConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(80);
+        factory.addAdditionalTomcatConnectors(connector);
+    }
+}
