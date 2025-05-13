b309d69a4286c0b154c18db967c8c0734eab2c6c
Valerii <sorvall@mail.ru>
2025-05-13 14:17:09 +0300

    http->https redirect

--- src/main/java/ru/sorokin/gateway/configuration/SecurityConfig.java
+++ src/main/java/ru/sorokin/gateway/configuration/SecurityConfig.java
@@ -1,16 +1,9 @@
-/*
-
 package ru.sorokin.gateway.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,30 +12,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/dashboard/**").authenticated()
-                        .anyRequest().permitAll()
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().permitAll()
                 )
-                .formLogin(Customizer.withDefaults()) // современный способ вместо formLogin()
-                .httpBasic(Customizer.withDefaults()); // если хочешь поддерживать curl-проверки
+                .csrf(AbstractHttpConfigurer::disable)
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure()
+                );
 
         return http.build();
     }
-
-    @Bean
-    public UserDetailsService users() {
-        String username = System.getenv("DASHBOARD_USER");
-        String password = System.getenv("DASHBOARD_PASS");
-        return new InMemoryUserDetailsManager(
-                User.withUsername(username)
-                        .password("{noop}" + password)
-                        .roles("ADMIN")
-                        .build()
-        );
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance(); // простой вариант без шифрования
-    }
-}*/
+}
