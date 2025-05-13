bcf20f2cb741019b83e28fe1b8b7f9ef8a2fb8a4
Valerii <sorvall@mail.ru>
2025-05-13 12:32:39 +0300

    http->https redirect

--- src/main/java/ru/sorokin/gateway/configuration/ServerConfig.java
+++ src/main/java/ru/sorokin/gateway/configuration/ServerConfig.java
@@ -1,6 +1,9 @@
 package ru.sorokin.gateway.configuration;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,18 +14,29 @@ public class ServerConfig {
 
     @Bean
     public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.setContextPath("");
-        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                // Добавляем правило: все HTTP-запросы должны перенаправляться на HTTPS
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
         return tomcat;
     }
 
-    private Connector createHttpConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8080);  // Внутренний порт
+        connector.setPort(8080); // В контейнере это порт, маппится на 80 хоста
         connector.setSecure(false);
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(443); // Перенаправляет на HTTPS
         return connector;
     }
 }
