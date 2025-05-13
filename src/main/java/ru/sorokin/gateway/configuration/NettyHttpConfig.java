42372670bddcefd0edb8023046ff6670826eb327
Valerii <sorvall@mail.ru>
2025-05-13 16:18:25 +0300

    http->https redirect

--- src/main/java/ru/sorokin/gateway/configuration/NettyHttpConfig.java
+++ src/main/java/ru/sorokin/gateway/configuration/NettyHttpConfig.java
@@ -1,27 +1,20 @@
 package ru.sorokin.gateway.configuration;
 
-import io.netty.channel.ChannelOption;
-import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.netty.http.server.HttpServer;
 
 @Configuration
 public class NettyHttpConfig {
-
     @Bean
-    public WebServerFactoryCustomizer<NettyReactiveWebServerFactory> nettyCustomizer() {
-        return factory -> factory.addServerCustomizers(httpServer -> {
-            return HttpServer.create()
-                    .port(80)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true)
-                    .route(routes -> routes.route(req -> true, (request, response) -> {
-                        String host = request.requestHeaders().get("Host");
-                        return response.status(301)
-                                .header("Location", "https://" + host + request.uri())
-                                .send();
-                    }));
-        });
+    public NettyServerCustomizer nettyServerCustomizer() {
+        return httpServer -> httpServer
+                .port(80)
+                .handle((req, res) -> {
+                    String host = req.requestHeaders().get("Host");
+                    return res.status(301)
+                            .header("Location", "https://" + host + req.uri())
+                            .send();
+                });
     }
 }
