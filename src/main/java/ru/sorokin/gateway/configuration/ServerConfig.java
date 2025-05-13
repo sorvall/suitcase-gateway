84fecabe04aa7534de27fed73aaae456ca2ad3aa
Valerii <sorvall@mail.ru>
2025-05-13 11:57:47 +0300

    http->https redirect

---
+++ src/main/java/ru/sorokin/gateway/configuration/ServerConfig.java
@@ -0,0 +1,27 @@
+package ru.sorokin.gateway.configuration;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        return tomcat;
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);  // Важно: редирект на HTTPS порт
+        return connector;
+    }
+}
